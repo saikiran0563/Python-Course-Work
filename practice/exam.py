9b9332a8e1e680359f3a4dc352e7c0ac65148bd1
saikiran0563 <sai402952@gmail.com>
2025-11-18 15:13:08 +0530

    Updated class work files

---
+++ practice/exam.py
@@ -0,0 +1,55 @@
+word = "banana" 
+print(word.count("a"))  # Output: 3
+
+line = "python is tough"
+replaced_line = line.replace("tough", "easy")
+print(replaced_line)  # Output: python is easy
+
+sentence = "learn python"
+print(len(sentence))
+
+marks = [45,67,89,32]
+marks.sort()
+print(marks)  # Output: [32, 45, 67, 89
+
+numbers = [10,20,30]
+numbers.append(40)
+print(numbers)  # Output: [10, 20, 30, 40]
+
+colors = ["red", "blue", "green"]
+colors.remove("blue")
+print(colors)  # Output: ['red', 'green']
+
+values = [1,2,3,2,1]
+print(values.count(2)) # Output: 2
+
+scores = [100, 90, 80]
+scores.reverse()
+print(scores)  # Output: [80, 90, 100]
+
+data = [5,3,9,1]
+print(max(data))  # Output: 9
+
+group = ("A","B","C","D")
+print(group[2:])
+ # Output: ('C', 'D')
+
+a  = {1,2,3}
+b = {2,3,4,}
+print(a.union(b))  # Output: {1, 2, 3, 4}
+
+x = {1,2,3,}
+y = {2,4}
+print(x.intersection(y))  # Output: {2}
+
+numbers = {5,10,15,}
+numbers.add(20)
+print(numbers)  # Output: {5, 10, 15, 20}
+
+items = {1,2,3,4}
+items.remove(3)
+print(items)  # Output: {1, 2, 4}
+
+marks = {"ravi" : 90, "anu" : 85}
+marks
+print(marks)
